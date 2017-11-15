c1ddc8d87e1bb8894ba6d766f0f0f6dbb8d4ca02
Charles LECALIER <clecalie@e2r9p3.42.fr>
2017-11-15 17:44:44 +0100

    check content

--- check_tetrim.c
+++ check_tetrim.c
@@ -6,11 +6,35 @@
 /*   By: clecalie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 16:28:19 by clecalie          #+#    #+#             */
-/*   Updated: 2017/11/15 16:36:16 by clecalie         ###   ########.fr       */
+/*   Updated: 2017/11/15 17:44:37 by clecalie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int		check_square(char *str)
+{
+	int		j;
+	int		i;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			if (j != 4 && j != 1)
+			{
+				printf("j: %d\n", j);
+				return (0);
+			}
+			j = 0;
+		}
+		i++;
+		j++;
+	}
+	return (1);
+}
 
 int		check_jumpline(char *str)
 {
@@ -21,6 +45,11 @@ int		check_jumpline(char *str)
 	count = 0;
 	while (str[i])
 	{
+		if (str[i + 1] == '\0' && str[i] != '\n')
+			return (0);
+		if (str[i] == '\n' && str[i + 1] && str[i + 1] == '\n'
+				&& str[i + 2] && str[i + 2] == '\n')
+			return (0);
 		if (str[i] == '\n')
 			count++;
 		i++;
@@ -46,7 +75,7 @@ int		check_charac(char *str)
 
 int		check_content(char *str)
 {
-	if (check_charac(str) && check_jumpline(str))
+	if (check_charac(str) && check_jumpline(str) && check_square(str))
 		return (1);
 	return (0);
 }
